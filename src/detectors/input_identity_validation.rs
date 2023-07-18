1683f7e7d7f49aa46aea6c77cd4681afcc79b67f
Camden Smallwood <camden.smallwood@gmail.com>
2023-07-18 19:54:37 -0400

    Clean up initial `input_identity_validation` detector

--- src/detectors/input_identity_validation.rs
+++ src/detectors/input_identity_validation.rs
@@ -1,8 +1,11 @@
+use crate::{
+    error::Error,
+    project::Project,
+    visitor::{AstVisitor, BlockContext, FnContext, MatchExprContext, ModuleContext},
+};
 use std::{collections::HashMap, path::PathBuf};
-
+use sway_ast::{FnArg, FnArgs};
 use sway_types::{Span, Spanned};
-
-use crate::visitor::AstVisitor;
 
 #[derive(Default)]
 pub struct InputIdentityValidationVisitor {
@@ -28,25 +31,16 @@ struct BlockState {
 }
 
 impl AstVisitor for InputIdentityValidationVisitor {
-    fn visit_module(
-        &mut self,
-        context: &crate::visitor::ModuleContext,
-        _project: &mut crate::project::Project,
-    ) -> Result<(), crate::error::Error> {
+    fn visit_module(&mut self, context: &ModuleContext, _project: &mut Project) -> Result<(), Error> {
         // Create the module state
         if !self.module_states.contains_key(context.path) {
-            self.module_states
-                .insert(context.path.into(), ModuleState::default());
+            self.module_states.insert(context.path.into(), ModuleState::default());
         }
 
         Ok(())
     }
 
-    fn visit_fn(
-        &mut self,
-        context: &crate::visitor::FnContext,
-        _project: &mut crate::project::Project,
-    ) -> Result<(), crate::error::Error> {
+    fn visit_fn(&mut self, context: &FnContext, _project: &mut Project) -> Result<(), Error> {
         // Get the module state
         let module_state = self.module_states.get_mut(context.path).unwrap();
 
@@ -54,56 +48,53 @@ impl AstVisitor for InputIdentityValidationVisitor {
         let fn_signature = context.item_fn.fn_signature.span();
         let fn_state = module_state.fn_states.entry(fn_signature).or_insert(FnState::default());
 
+        // Check function arguments for `Address`, `ContractId` or `Identity` types and queue them to be checked
+        let mut check_for_identity_argument = |arg: &FnArg| {
+            match arg.ty.span().as_str() {
+                "Address" => {
+                    fn_state.address_checks.insert(arg.pattern.span(), false);
+                }
+
+                "ContractId" => {
+                    fn_state.contract_id_checks.insert(arg.pattern.span(), false);
+                }
+
+                "Identity" => {
+                    fn_state.identity_checks.insert(arg.pattern.span(), false);
+                }
+
+                _ => {}
+            }
+        };
+
         match &context.item_fn.fn_signature.arguments.inner {
-            sway_ast::FnArgs::Static(args) => {
+            FnArgs::Static(args) => {
                 for arg in args.value_separator_pairs.iter() {
-                    match arg.0.ty.span().as_str() {
-                        "Identity" => {
-                            fn_state.identity_checks.insert(arg.0.pattern.span(), false);
-                        }
-
-                        "ContractId" => {
-                            fn_state.contract_id_checks.insert(arg.0.pattern.span(), false);
-                        }
-
-                        "Address" => {
-                            fn_state.address_checks.insert(arg.0.pattern.span(), false);
-                        }
-
-                        _ => {}
-                    }
+                    check_for_identity_argument(&arg.0);
                 }
 
                 if let Some(arg) = args.final_value_opt.as_ref() {
-                    match arg.ty.span().as_str() {
-                        "Identity" => {
-                            fn_state.identity_checks.insert(arg.pattern.span(), false);
-                        }
-
-                        "ContractId" => {
-                            fn_state.contract_id_checks.insert(arg.pattern.span(), false);
-                        }
-
-                        "Address" => {
-                            fn_state.address_checks.insert(arg.pattern.span(), false);
-                        }
-
-                        _ => {}
-                    }
+                    check_for_identity_argument(arg);
                 }
             }
             
-            sway_ast::FnArgs::NonStatic { self_token, ref_self, mutable_self, args_opt } => todo!(),
+            FnArgs::NonStatic { args_opt: Some(args), .. } => {
+                for arg in args.1.value_separator_pairs.iter() {
+                    check_for_identity_argument(&arg.0);
+                }
+
+                if let Some(arg) = args.1.final_value_opt.as_ref() {
+                    check_for_identity_argument(arg);
+                }
+            }
+
+            _ => {}
         }
 
         Ok(())
     }
 
-    fn leave_fn(
-        &mut self,
-        context: &crate::visitor::FnContext,
-        project: &mut crate::project::Project,
-    ) -> Result<(), crate::error::Error> {
+    fn leave_fn(&mut self, context: &FnContext, project: &mut Project) -> Result<(), Error> {
         // Get the module state
         let module_state = self.module_states.get(context.path).unwrap();
 
@@ -125,11 +116,7 @@ impl AstVisitor for InputIdentityValidationVisitor {
         Ok(())
     }
 
-    fn visit_block(
-        &mut self,
-        context: &crate::visitor::BlockContext,
-        _project: &mut crate::project::Project,
-    ) -> Result<(), crate::error::Error> {
+    fn visit_block(&mut self, context: &BlockContext, _project: &mut Project) -> Result<(), Error> {
         // Get the module state
         let module_state = self.module_states.get_mut(context.path).unwrap();
 
@@ -141,19 +128,13 @@ impl AstVisitor for InputIdentityValidationVisitor {
         let block_span = context.block.span();
 
         if !fn_state.block_states.contains_key(&block_span) {
-            fn_state
-                .block_states
-                .insert(block_span, BlockState::default());
+            fn_state.block_states.insert(block_span, BlockState::default());
         }
 
         Ok(())
     }
 
-    fn visit_match_expr(
-        &mut self,
-        context: &crate::visitor::MatchExprContext,
-        project: &mut crate::project::Project,
-    ) -> Result<(), crate::error::Error> {
+    fn visit_match_expr(&mut self, context: &MatchExprContext, _project: &mut Project) -> Result<(), Error> {
         println!("{:#?}", context.expr);
         Ok(())
     }
