476b47cdee7f2cbbb5af9dbe5c23c879192f3654
Camden Smallwood <camden.smallwood@gmail.com>
2023-08-08 08:57:04 -0400

    Some work on `input_identity_validation` detector

--- src/detectors/input_identity_validation.rs
+++ src/detectors/input_identity_validation.rs
@@ -1,10 +1,10 @@
 use crate::{
     error::Error,
     project::Project,
-    visitor::{AstVisitor, BlockContext, FnContext, MatchExprContext, ModuleContext},
+    visitor::{AstVisitor, BlockContext, FnContext, ModuleContext, StatementContext},
 };
-use std::{collections::HashMap, path::PathBuf};
-use sway_ast::{FnArg, FnArgs};
+use std::{collections::HashMap, path::PathBuf, ops::ControlFlow};
+use sway_ast::{FnArg, FnArgs, IfExpr, IfCondition, expr::LoopControlFlow};
 use sway_types::{Span, Spanned};
 
 #[derive(Default)]
@@ -27,7 +27,7 @@ struct FnState {
 
 #[derive(Default)]
 struct BlockState {
-    // TODO
+    variables: Vec<Span>,
 }
 
 impl AstVisitor for InputIdentityValidationVisitor {
@@ -134,8 +134,177 @@ impl AstVisitor for InputIdentityValidationVisitor {
         Ok(())
     }
 
-    fn visit_match_expr(&mut self, context: &MatchExprContext, _project: &mut Project) -> Result<(), Error> {
-        println!("{:#?}", context.expr);
+    fn visit_statement(&mut self, context: &StatementContext, project: &mut Project) -> Result<(), Error> {
+        // Get the module state
+        let module_state = self.module_states.get_mut(context.path).unwrap();
+
+        // Get the function state
+        let fn_signature = context.item_fn.fn_signature.span();
+        let fn_state = module_state.fn_states.get_mut(&fn_signature).unwrap();
+
+        // Get the block state
+        let block_span = context.blocks.last().unwrap();
+        let block_state = fn_state.block_states.get_mut(&block_span).unwrap();
+
+        // Store variable bindings declared in the current block
+        if let sway_ast::Statement::Let(item_let) = context.statement {
+            match &item_let.pattern {
+                //
+                // TODO: handle other patterns
+                //
+
+                sway_ast::Pattern::Var { name, .. } => {
+                    block_state.variables.push(name.span());
+                }
+
+                _ => {}
+            }
+
+            return Ok(());
+        }
+
+        // Only check expression statements
+        let sway_ast::Statement::Expr { expr, .. } = context.statement else {
+            return Ok(())
+        };
+
+        match expr {
+            sway_ast::Expr::Match { value, branches, .. } => {
+                //
+                // TODO: check for the following pattern
+                //
+                // match to {
+                //     Identity::Address(x) => require(x != Address::from(ZERO_B256), "Zero address"),
+                //     Identity::ContractId(x) => require(x != ContractId::from(ZERO_B256), "Zero contract id"),
+                // }
+                //
+
+                // Check if `value` is a variable declaration, skip if so
+                for block_span in context.blocks.iter().rev() {
+                    let block_state = fn_state.block_states.get(block_span).unwrap();
+
+                    if block_state.variables.iter().any(|v| v.as_str() == value.span().as_str()) {
+                        return Ok(())
+                    }
+                }
+
+                //
+                // TODO: check if `value` is a parameter of type `Identity`
+                //
+
+                //
+                // TODO: check `branches` for `Identity::Address` and `Identity::ContractId` zero value checks
+                //
+            }
+            
+            sway_ast::Expr::If(if_expr) => {
+                //
+                // TODO: check for the following pattern
+                //
+                // if let Identity::Address(x) = to {
+                //     require(x != Address::from(ZERO_B256), "Zero address");
+                // } else if let Identity::ContractId(x) = to {
+                //     require(x != ContractId::from(ZERO_B256), "Zero contract id");
+                // }
+                //
+
+                let IfExpr {
+                    condition: IfCondition::Let {
+                        lhs: if_let_condition_lhs,
+                        rhs: if_let_condition_rhs,
+                        ..
+                    },
+                    then_block: if_let_then_block,
+                    else_opt: Some((_, LoopControlFlow::Continue(else_if_expr))),
+                    ..
+                } = if_expr else {
+                    return Ok(());
+                };
+
+                // Check if `if_let_condition_rhs` is a variable declaration, skip if so
+                for block_span in context.blocks.iter().rev() {
+                    let block_state = fn_state.block_states.get(block_span).unwrap();
+
+                    if block_state.variables.iter().any(|v| v.as_str() == if_let_condition_rhs.span().as_str()) {
+                        return Ok(())
+                    }
+                }
+
+                //
+                // TODO: check if `if_let_condition_rhs` is a parameter of type `Identity`
+                //
+
+                //
+                // TODO: check `if_let_then_block` statements for `Identity::Address` and `Identity::ContractId` zero value checks
+                //
+                
+                let IfExpr {
+                    condition: IfCondition::Let {
+                        lhs: else_if_let_condition_lhs,
+                        rhs: else_if_let_condition_rhs,
+                        ..
+                    },
+                    then_block: else_if_let_then_block,
+                    ..
+                } = else_if_expr.as_ref() else {
+                    return Ok(());
+                };
+
+                // Check if `else_if_let_condition_rhs` is a variable declaration, skip if so
+                for block_span in context.blocks.iter().rev() {
+                    let block_state = fn_state.block_states.get(block_span).unwrap();
+
+                    if block_state.variables.iter().any(|v| v.as_str() == else_if_let_condition_rhs.span().as_str()) {
+                        return Ok(())
+                    }
+                }
+                
+                //
+                // TODO: check if `else_if_let_condition_rhs` is a parameter of type `Identity`
+                //
+
+                //
+                // TODO: check `else_if_let_then_block` statements for `Identity::Address` and `Identity::ContractId` zero value checks
+                //
+            }
+
+            sway_ast::Expr::FuncApp { func, args } => {
+                // Only check require calls
+                if func.span().as_str() != "require" {
+                    return Ok(())
+                }
+
+                //
+                // TODO: check for the following pattern
+                //
+                // require(
+                //     match to {
+                //         Identity::Address(x) => x != Address::from(ZERO_B256),
+                //         Identity::ContractId(x) => x != ContractId::from(ZERO_B256),
+                //     },
+                //     "Zero identity"
+                // );
+                //
+
+                //
+                // TODO: check for the following pattern
+                //
+                // require(
+                //     if let Identity::Address(x) = to {
+                //         x != Address::from(ZERO_B256)
+                //     } else if let Identity::ContractId(x) = to {
+                //         x != ContractId::from(ZERO_B256)
+                //     } else {
+                //         true
+                //     },
+                //     "Zero identity"
+                // );
+                //        
+            }
+
+            _ => {}
+        }
+
         Ok(())
     }
 }
