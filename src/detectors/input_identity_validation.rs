5c1dcbfdcd60273b2ffaa97e5d3697eaf0f90d1a
Camden Smallwood <camden.smallwood@gmail.com>
2023-08-09 03:01:21 -0400

    Some more work on `input_identity_validation` detector

--- src/detectors/input_identity_validation.rs
+++ src/detectors/input_identity_validation.rs
@@ -5,7 +5,7 @@ use crate::{
 };
 use std::{collections::HashMap, path::PathBuf};
 use sway_ast::{
-    expr::LoopControlFlow, Expr, FnArg, FnArgs, IfCondition, IfExpr, Pattern, Statement,
+    expr::LoopControlFlow, Expr, FnArg, FnArgs, IfCondition, IfExpr, Pattern, Statement, PathExpr,
 };
 use sway_types::{Span, Spanned};
 
@@ -22,9 +22,9 @@ struct ModuleState {
 #[derive(Default)]
 struct FnState {
     block_states: HashMap<Span, BlockState>,
-    identity_checks: HashMap<Span, bool>,
-    contract_id_checks: HashMap<Span, bool>,
     address_checks: HashMap<Span, bool>,
+    contract_id_checks: HashMap<Span, bool>,
+    identity_checks: HashMap<Span, (bool, bool)>,
 }
 
 #[derive(Default)]
@@ -62,7 +62,7 @@ impl AstVisitor for InputIdentityValidationVisitor {
                 }
 
                 "Identity" => {
-                    fn_state.identity_checks.insert(arg.pattern.span(), false);
+                    fn_state.identity_checks.insert(arg.pattern.span(), (false, false));
                 }
 
                 _ => {}
@@ -104,14 +104,49 @@ impl AstVisitor for InputIdentityValidationVisitor {
         let fn_signature = context.item_fn.fn_signature.span();
         let fn_state = module_state.fn_states.get(&fn_signature).unwrap();
 
-        // Check for any unchecked parameters
-        for (parameter_span, parameter_checked) in fn_state.identity_checks.iter() {
-            if !parameter_checked {
-                project.report.borrow_mut().add_entry(
-                    context.path,
-                    project.span_to_line(context.path, parameter_span)?,
-                    format!("Parameter `{}` is not checked for a zero value", parameter_span.as_str()),
-                );
+        let create_report_entry = |parameter_span: &Span| -> Result<(), Error> {
+            project.report.borrow_mut().add_entry(
+                context.path,
+                project.span_to_line(context.path, parameter_span)?,
+                format!(
+                    "The `{}` function does not check its `{}` parameter for a zero value.",
+                    if let Some(item_impl) = context.item_impl.as_ref() {
+                        format!(
+                            "{}::{}",
+                            item_impl.ty.span().as_str(),
+                            context.item_fn.fn_signature.name.as_str(),
+                        )
+                    } else {
+                        format!(
+                            "{}",
+                            context.item_fn.fn_signature.name.as_str(),
+                        )
+                    },
+                    parameter_span.as_str(),
+                ),
+            );
+
+            Ok(())
+        };
+
+        // Check for any unchecked parameters of type `Address`
+        for (parameter_span, address_checked) in fn_state.address_checks.iter() {
+            if !address_checked {
+                create_report_entry(parameter_span)?;
+            }
+        }
+
+        // Check for any unchecked parameters of type `ContractId`
+        for (parameter_span, contract_id_checked) in fn_state.contract_id_checks.iter() {
+            if !contract_id_checked {
+                create_report_entry(parameter_span)?;
+            }
+        }
+
+        // Check for any unchecked parameters of type `Identity`
+        for (parameter_span, (address_checked, contract_id_checked)) in fn_state.identity_checks.iter() {
+            if !address_checked || !contract_id_checked {
+                create_report_entry(parameter_span)?;
             }
         }
 
@@ -168,13 +203,13 @@ impl AstVisitor for InputIdentityValidationVisitor {
 
         // Only check expression statements
         let Statement::Expr { expr, .. } = context.statement else {
-            return Ok(())
+            return Ok(());
         };
 
         match expr {
             Expr::Match { value, branches, .. } => {
                 //
-                // TODO: check for the following pattern
+                // Check for the following pattern:
                 //
                 // match to {
                 //     Identity::Address(x) => require(x != Address::from(ZERO_B256), "Zero address"),
@@ -192,16 +227,92 @@ impl AstVisitor for InputIdentityValidationVisitor {
                 }
 
                 // Check if `value` is a parameter of type `Identity`
-                if let Some((_, identity_checked)) = fn_state.identity_checks.iter_mut().find(|(x, _)| x.as_str() == value.span().as_str()) {
+                if let Some((_, (address_checked, contract_id_checked))) = fn_state.identity_checks.iter_mut().find(|(x, _)| x.as_str() == value.span().as_str()) {
                     //
-                    // TODO: check `branches` for `Identity::Address` and `Identity::ContractId` zero value checks
+                    // Check `branches` for `Identity::Address` and `Identity::ContractId` zero value checks
                     //
+
+                    for branch in branches.inner.iter() {
+                        let Pattern::Constructor { path, args } = &branch.pattern else { continue };
+                        let "Identity" = path.prefix.name.as_str() else { continue };
+                        let Some(suffix) = path.suffix.last() else { continue };
+                        
+                        let mut ident = None;
+
+                        match args.inner.final_value_opt.as_ref() {
+                            Some(arg) => match arg.as_ref() {
+                                Pattern::AmbiguousSingleIdent(arg) => ident = Some(arg),
+                                _ => {}
+                            }
+
+                            None => match args.inner.value_separator_pairs.first() {
+                                Some(arg) => match &arg.0 {
+                                    Pattern::AmbiguousSingleIdent(arg) => ident = Some(arg),
+                                    _ => {}
+                                }
+
+                                None => {}
+                            }
+                        }
+
+                        let Some(ident) = ident else { continue };
+
+                        let mut check_expr = |expr: &Expr| {
+                            let Expr::FuncApp { func, args } = expr else { return };
+                            let "require" = func.span().as_str() else { return };
+
+                            let input = match args.inner.value_separator_pairs.first() {
+                                Some(arg) => &arg.0,
+                                None => match args.inner.final_value_opt.as_ref() {
+                                    Some(arg) => arg.as_ref(),
+                                    None => return,
+                                }
+                            };
+
+                            let Expr::NotEqual { lhs, rhs, .. } = input else { return };
+
+                            let zero_value = if lhs.span().as_str() == ident.span().as_str() {
+                                rhs.as_ref()
+                            } else if rhs.span().as_str() == ident.span().as_str() {
+                                lhs.as_ref()
+                            } else {
+                                return;
+                            };
+
+                            let Expr::FuncApp { func, args } = zero_value else { return };
+                            if func.span().as_str() != format!("{}::from", suffix.1.name.as_str()) { return; }
+                            if args.span().as_str() != "(ZERO_B256)" { return; }
+
+                            match suffix.1.name.as_str() {
+                                "Address" => *address_checked = true,
+                                "ContractId" => *contract_id_checked = true,
+                                _ => {}
+                            }
+                        };
+
+                        match &branch.kind {
+                            sway_ast::MatchBranchKind::Block { block, .. } => {
+                                for statement in block.inner.statements.iter() {
+                                    let Statement::Expr { expr, .. } = statement else { continue };
+                                    check_expr(expr);
+                                }
+
+                                if let Some(expr) = block.inner.final_expr_opt.as_ref() {
+                                    check_expr(expr);
+                                }
+                            }
+
+                            sway_ast::MatchBranchKind::Expr { expr, .. } => {
+                                check_expr(expr);
+                            }
+                        }
+                    }
                 }
             }
             
             Expr::If(if_expr) => {
                 //
-                // TODO: check for the following pattern
+                // Check for the following pattern:
                 //
                 // if let Identity::Address(x) = to {
                 //     require(x != Address::from(ZERO_B256), "Zero address");
@@ -210,69 +321,133 @@ impl AstVisitor for InputIdentityValidationVisitor {
                 // }
                 //
 
-                let IfExpr {
-                    condition: IfCondition::Let {
-                        lhs: if_let_condition_lhs,
-                        rhs: if_let_condition_rhs,
+                let mut next_if_expr = Some(if_expr);
+
+                'if_expr_check: while let Some(if_expr) = next_if_expr {
+                    let IfExpr {
+                        condition: IfCondition::Let { lhs, rhs, .. },
+                        then_block,
+                        else_opt,
                         ..
-                    },
-                    then_block: if_let_then_block,
-                    else_opt: Some((_, LoopControlFlow::Continue(else_if_expr))),
-                    ..
-                } = if_expr else {
-                    return Ok(());
-                };
+                    } = if_expr else {
+                        break;
+                    };
 
-                // Check if `if_let_condition_rhs` is a variable declaration, skip if so
-                for block_span in context.blocks.iter().rev() {
-                    let block_state = fn_state.block_states.get(block_span).unwrap();
+                    // Check if `rhs` is a variable declaration, skip if so
+                    for block_span in context.blocks.iter().rev() {
+                        let block_state = fn_state.block_states.get(block_span).unwrap();
+    
+                        if block_state.variables.iter().any(|v| v.as_str() == rhs.span().as_str()) {
+                            // Jump to the next if expression if available
+                            if let Some((_, LoopControlFlow::Continue(else_if_expr))) = else_opt {
+                                next_if_expr = Some(else_if_expr.as_ref());
+                            } else {
+                                next_if_expr = None;
+                            }
 
-                    if block_state.variables.iter().any(|v| v.as_str() == if_let_condition_rhs.span().as_str()) {
-                        return Ok(());
+                            continue 'if_expr_check;
+                        }
                     }
-                }
+    
+                    // Check if `rhs` is a parameter of type `Identity`
+                    if let Some((_, (address_checked, contract_id_checked))) = fn_state.identity_checks.iter_mut().find(|(x, _)| x.as_str() == rhs.span().as_str()) {
+                        //
+                        // Check `then_block` statements for `Identity::Address` and `Identity::ContractId` zero value checks
+                        //
 
-                // Check if `if_let_condition_rhs` is a parameter of type `Identity`
-                if let Some((_, identity_checked)) = fn_state.identity_checks.iter_mut().find(|(x, _)| x.as_str() == if_let_condition_rhs.span().as_str()) {
-                    //
-                    // TODO: check `if_let_then_block` statements for `Identity::Address` and `Identity::ContractId` zero value checks
-                    //
-                }
-                
-                let IfExpr {
-                    condition: IfCondition::Let {
-                        lhs: else_if_let_condition_lhs,
-                        rhs: else_if_let_condition_rhs,
-                        ..
-                    },
-                    then_block: else_if_let_then_block,
-                    ..
-                } = else_if_expr.as_ref() else {
-                    return Ok(());
-                };
+                        let Pattern::Constructor { path, args } = lhs.as_ref() else { continue };
+                        let "Identity" = path.prefix.name.as_str() else { continue };
+                        let Some(suffix) = path.suffix.last() else { continue };
+                        
+                        let mut ident = None;
 
-                // Check if `else_if_let_condition_rhs` is a variable declaration, skip if so
-                for block_span in context.blocks.iter().rev() {
-                    let block_state = fn_state.block_states.get(block_span).unwrap();
+                        match args.inner.final_value_opt.as_ref() {
+                            Some(arg) => match arg.as_ref() {
+                                Pattern::AmbiguousSingleIdent(arg) => ident = Some(arg),
+                                _ => {}
+                            }
 
-                    if block_state.variables.iter().any(|v| v.as_str() == else_if_let_condition_rhs.span().as_str()) {
-                        return Ok(());
+                            None => match args.inner.value_separator_pairs.first() {
+                                Some(arg) => match &arg.0 {
+                                    Pattern::AmbiguousSingleIdent(arg) => ident = Some(arg),
+                                    _ => {}
+                                }
+
+                                None => {}
+                            }
+                        }
+
+                        let Some(ident) = ident else { continue };
+
+                        let mut check_expr = |expr: &Expr| {
+                            let Expr::FuncApp { func, args } = expr else { return };
+                            let "require" = func.span().as_str() else { return };
+
+                            let input = match args.inner.value_separator_pairs.first() {
+                                Some(arg) => &arg.0,
+                                None => match args.inner.final_value_opt.as_ref() {
+                                    Some(arg) => arg.as_ref(),
+                                    None => return,
+                                }
+                            };
+
+                            let Expr::NotEqual { lhs, rhs, .. } = input else { return };
+
+                            let zero_value = if lhs.span().as_str() == ident.span().as_str() {
+                                rhs.as_ref()
+                            } else if rhs.span().as_str() == ident.span().as_str() {
+                                lhs.as_ref()
+                            } else {
+                                return;
+                            };
+
+                            let Expr::FuncApp { func, args } = zero_value else { return };
+                            if func.span().as_str() != format!("{}::from", suffix.1.name.as_str()) { return; }
+                            if args.span().as_str() != "(ZERO_B256)" { return; }
+
+                            match suffix.1.name.as_str() {
+                                "Address" => *address_checked = true,
+                                "ContractId" => *contract_id_checked = true,
+                                _ => {}
+                            }
+                        };
+
+                        for statement in then_block.inner.statements.iter() {
+                            let Statement::Expr { expr, .. } = statement else { continue };
+                            check_expr(expr);
+                        }
+
+                        if let Some(expr) = then_block.inner.final_expr_opt.as_ref() {
+                            check_expr(expr);
+                        }
                     }
-                }
-                
-                // Check if `else_if_let_condition_rhs` is a parameter of type `Identity`
-                if let Some((_, identity_checked)) = fn_state.identity_checks.iter_mut().find(|(x, _)| x.as_str() == else_if_let_condition_rhs.span().as_str()) {
-                    //
-                    // TODO: check `else_if_let_then_block` statements for `Identity::Address` and `Identity::ContractId` zero value checks
-                    //
+
+                    // Jump to the next if expression if available
+                    if let Some((_, LoopControlFlow::Continue(else_if_expr))) = else_opt {
+                        next_if_expr = Some(else_if_expr.as_ref());
+                    } else {
+                        next_if_expr = None;
+                    }
                 }
             }
-
+            
             Expr::FuncApp { func, args } => {
                 // Only check require calls
                 if func.span().as_str() != "require" {
                     return Ok(());
                 }
+
+                //
+                // TODO: check for the following pattern
+                //
+                // require(x != Address::from(ZERO_B256), "Zero address");
+                //
+
+                //
+                // TODO: check for the following pattern
+                //
+                // require(x != ContractId::from(ZERO_B256), "Zero contract id");
+                //
 
                 //
                 // TODO: check for the following pattern
